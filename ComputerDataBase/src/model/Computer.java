5e25316782ab91c417415c21bf507fff3efe0e7a
arthur <arthur.serre@gmail.com>
2017-09-11 15:29:52 +0200

    maj architecture

---
+++ ComputerDataBase/src/model/Computer.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Computer {
+	private int id;
+	private String name;
+	private String introduced;
+	private String discontinued;
+	private String companyId;
+	
+	public Computer() {}
+	
+	public Computer(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIntroduced() {
+		return introduced;
+	}
+
+	public void setIntroduced(String introduced) {
+		this.introduced = introduced;
+	}
+
+	public String getDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(String discontinued) {
+		this.discontinued = discontinued;
+	}
+
+	public String getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+}
